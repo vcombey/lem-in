48153ded1780c42d431201fee251f1c4681738b3
vcombey <vcombey@vincents-macbook-pro.home>
2017-02-18 15:20:36 +0100

    debut du parsing et de la structure de donne

--- main.c
+++ main.c
@@ -11,8 +11,74 @@
 /* ************************************************************************** */
 
 #include "lem-in.h"
+#include "libft/libft.h"
+
+int		is_a_number(char *line)
+{
+	while (*line)
+	{
+		if ((*line > 57) || (*line < 48))
+			return (0);
+	}
+	return (1);
+}
+
+void	secure_get_next_line(char **line)
+{
+	if (get_next_line(0, line) == -1)
+		exit(1);
+}
+
+void	get_room(char *line, t_ant *a)
+{
+	char	**s;
+
+	s = ft_strsplit(line, ' ');
+	if ((ft_strstrlen(s) != 3) || (!(is_a_number(s[1]))) || (!(is_a_number(s[2]))))
+		exit(1);
+
+}
 
 int		main()
 {
-	while (get_next_line(
+	char	*line;
+	t_ant	*a;
+
+	a = (t_ant*)ft_memalloc(sizeof(t_ant));
+	secure_get_next_line(&line);
+	if (!(is_a_number(line)))
+		exit(1);
+	a->nb = ft_atoi(line);
+	while (1)
+	{
+		secure_get_next_line(&line);
+		if (ft_strequ(line, "##start"))
+			get_start(line, a);
+		if (ft_strequ(line, "##end"))
+			get_end(line, a);
+		if (ft_strchr(line, '-'))
+			break;
+		if (line[0] == '#')
+			;
+		else
+			get_room(line, a);
+		free(line);
+	}
+	get_liason(line, a);
+	while (get_next_line(0, &line))
+	{
+		get_liason(line, a);
+		free(line);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
