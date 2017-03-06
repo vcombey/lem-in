a4b8323b7622e5139653500ec00dd347507c579e
vcombey <vcombey@student.42.fr>
2017-03-06 13:41:52 +0100

    resolution du nombre de \n a la fin

--- display_solus.c
+++ display_solus.c
@@ -6,7 +6,7 @@
 /*   By: vcombey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/02 16:12:39 by vcombey           #+#    #+#             */
-/*   Updated: 2017/03/02 16:33:50 by vcombey          ###   ########.fr       */
+/*   Updated: 2017/03/06 13:36:38 by vcombey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,20 @@
 #include "ft_printf.h"
 #include <unistd.h>
 
-void	display_move(t_room *solus)
+int		display_move(t_room *solus)
 {
+	int		has_moved;
+
 	if (solus->next == NULL)
-		return ;
-	display_move(solus->next);
+		return (0);
+	has_moved = (solus->n != -1);
+	has_moved |= display_move(solus->next);
 	if (solus->n == -1)
-		return ;
+		return (has_moved);
 	ft_printf("L%d-%s ", solus->n, solus->next->name);
 	solus->next->n = solus->n;
 	solus->n = -1;
+	return (has_moved);
 }
 
 void	display_solus(t_room *solus, t_anthill a)
@@ -55,6 +59,7 @@ void	display_multi_solus(t_ways *ways, t_anthill a)
 	int		i;
 	int		n;
 	t_ways	*tmp;
+	int		has_moved;
 
 	n = room_len(ways->solus);
 	i = 1;
@@ -78,13 +83,15 @@ void	display_multi_solus(t_ways *ways, t_anthill a)
 	i = 1;
 	while (i < room_len(ways->solus))
 	{
+		has_moved = 0;
 		tmp = ways;
 		while (tmp)
 		{
-			display_move(tmp->solus);
+			has_moved |= display_move(tmp->solus);
 			tmp = tmp->next;
 		}
-		write(1, "\n", 1);
+		if (has_moved)
+			write(1, "\n", 1);
 		i++;
 	}
 }
